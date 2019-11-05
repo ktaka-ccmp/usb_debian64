5a12c7fc333b86b76f2a1943480299f7ade35b09
Kimitoshi Takahashi <ktaka@ccmp.jp>
2019-11-05 12:55:25 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-TOP_DIR=${PWD}
-IMG_DIR=${TOP_DIR}/Build/image/
-
 removable(){
 if  (readlink -f /sys/block/${1}/device|egrep -q usb) ; then
 	dev=/dev/${1}
@@ -40,9 +37,9 @@ partprobe
 mkfs.ext4 -F -L usbdebian ${dev}1
 make update
 
-grub-mkimage -o ${IMG_DIR}/boot/grub/core.img  ext2 part_msdos biosdisk -O i386-pc  -p ./image/boot/
-grub-mkdevicemap -v -m  ${IMG_DIR}/boot/grub/device.map
-grub-install --boot-directory=${IMG_DIR}/boot/ ${dev}
+mount -L usbdebian ./mnt
+grub-install --boot-directory=./mnt/boot/ ${dev}
+umount ./mnt
 
 sfdisk -A $dev
 
